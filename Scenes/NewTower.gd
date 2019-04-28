3ef2e27affcf7f9aad830980d259fcd3d8d23b16
Fabian Schober <fabian@imendo.at>
2019-04-28 11:23:29 +0200

    new controls

--- Scenes/NewTower.gd
+++ Scenes/NewTower.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-export var angular_speed = 60
+export var angular_speed = 90
 export var number = 1
 var wasd = false
 
@@ -13,9 +13,22 @@ func _ready():
 	$NewLaser.change_color(colorName)
 		
 func _process(delta):
+	var direction = Vector2.ZERO
 	if wasd && Input.is_key_pressed(KEY_D) || !wasd && Input.is_action_pressed("ui_right"):
-		rotate(angular_speed*delta)
+		direction.x += 1
 	if wasd && Input.is_key_pressed(KEY_A) || !wasd && Input.is_action_pressed("ui_left"):
-		rotate(-angular_speed*delta)
-	#if wasd && Input.is_key_pressed(KEY_W) || !wasd && Input.is_action_just_pressed("ui_up"):
-	#	$NewLaser.color += 1
+		direction.x -= 1
+	if wasd && Input.is_key_pressed(KEY_W) || !wasd && Input.is_action_pressed("ui_up"):
+		direction.y -=1
+	if wasd && Input.is_key_pressed(KEY_S) || !wasd && Input.is_action_pressed("ui_down"):
+		direction.y +=1
+	
+	if direction.length_squared() != 0:
+		var target_angle = direction.angle()
+		var diff_1 = fposmod(target_angle - rotation, 2 * PI)
+		var diff_2 = fposmod(rotation - target_angle, 2 * PI)
+
+		if diff_1 < diff_2:
+			rotation += min(diff_1, delta * angular_speed)
+		else:
+			rotation -= min(diff_2, delta * angular_speed)
