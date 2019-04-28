1a40e138487925fc3b927c1b8d28e5972c5d1f69
Benjamin Hackl <devel@benjamin-hackl.at>
2019-04-28 13:29:53 +0200

    "finish" HUD

--- Scenes/HUD.gd
+++ Scenes/HUD.gd
@@ -1,18 +1,24 @@
 extends CanvasLayer
 
 var lifes = 3
+var number_of_levels = 2
+
 var continue_select = false
 var mainmenu_select = false
 var restart_select = false
+var next_level_select = false
 var hover_time = 0
-var game_over_flag = false
+var level_ended = false
+
+var next_level = "Level_1"
 
 func _ready():
 	$PauseScreen.hide()
 	$GameOverScreen.hide()
+	$SuccessScreen.hide()
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel") and not game_over_flag:
+	if Input.is_action_just_pressed("ui_cancel") and not level_ended:
 		if not get_tree().paused:
 			get_tree().paused = true
 			$PauseScreen.show()
@@ -26,9 +32,14 @@ func _process(delta):
 	if mainmenu_select:
 		$PauseScreen/CenterContainer/VBoxContainer/ReturnMainLabel.set_rotation(PI/24 * sin(hover_time))
 		$GameOverScreen/CenterContainer/VBoxContainer/ReturnMainLabel.set_rotation(PI/24 * sin(hover_time))
+		$SuccessScreen/CenterContainer/VBoxContainer/ReturnMainLabel.set_rotation(PI/24 * sin(hover_time))
 		hover_time += 10*delta
 	if restart_select:
 		$GameOverScreen/CenterContainer/VBoxContainer/RestartLabel.set_rotation(PI/24 * sin(hover_time))
+		$SuccessScreen/CenterContainer/VBoxContainer/RestartLabel.set_rotation(PI/24 * sin(hover_time))
+		hover_time += 10*delta
+	if next_level_select:
+		$SuccessScreen/CenterContainer/VBoxContainer/NextLevelLabel.set_rotation(PI/24 * sin(hover_time))
 		hover_time += 10*delta
 
 func ContinueLabel_emph_start():
@@ -47,6 +58,7 @@ func ReturnMainLabel_emph_stop():
 	hover_time = 0
 	$PauseScreen/CenterContainer/VBoxContainer/ReturnMainLabel.set_rotation(0)
 	$GameOverScreen/CenterContainer/VBoxContainer/ReturnMainLabel.set_rotation(0)
+	$SuccessScreen/CenterContainer/VBoxContainer/ReturnMainLabel.set_rotation(0)
 
 func RestartLabel_emph_start():
 	restart_select = true
@@ -55,6 +67,16 @@ func RestartLabel_emph_stop():
 	restart_select = false
 	hover_time = 0
 	$GameOverScreen/CenterContainer/VBoxContainer/RestartLabel.set_rotation(0)
+	$SuccessScreen/CenterContainer/VBoxContainer/RestartLabel.set_rotation(0)
+
+func NextLevelLabel_emph_start():
+	next_level_select = true
+	
+func NextLevelLabel_emph_stop():
+	next_level_select = false
+	hover_time = 0
+	$SuccessScreen/CenterContainer/VBoxContainer/NextLevelLabel.set_rotation(0)
+
 
 func _on_ReturnMainLabel_gui_input(event):
 	if Input.is_action_just_pressed("ui_accept"):
@@ -75,18 +97,43 @@ func update_life_counter():
 			life_textures[2-k].hide()
 
 func game_over():
-	game_over_flag = true
+	level_ended = true
 	$GameOverScreen.show()
 	$GameOverScreen/CenterContainer/VBoxContainer/RestartLabel.grab_focus()
 
 func _on_missed_duck():
 	lifes -= 1
 	update_life_counter()
-	if lifes <= 0 and not game_over_flag and get_tree():
+	if lifes <= 0 and not level_ended and get_tree():
 		get_tree().paused = true
 		game_over()
 
 func _on_RestartLabel_gui_input(event):
 	if Input.is_action_just_pressed("ui_accept"):
 		get_tree().reload_current_scene()
+		get_tree().paused = false
+
+func _on_level_complete(level):
+	get_tree().paused = true
+	var success_text
+	if level < number_of_levels:
+		success_text = "Success in World " + str(level) + "!\n Continue to next level?"
+		$SuccessScreen/CenterContainer/VBoxContainer/NextLevelLabel.grab_focus()
+		next_level = "Level_" + str(level+1)
+	elif level == number_of_levels:
+		success_text = "Congratulations!\n You beat the game! :)"
+		$SuccessScreen/CenterContainer/VBoxContainer/NextLevelLabel.hide()
+		$SuccessScreen/CenterContainer/VBoxContainer/RestartLabel.grab_focus()
+	
+	$SuccessScreen/CenterContainer/VBoxContainer/NextLevelLabel.rect_pivot_offset = 0.5*$SuccessScreen/CenterContainer/VBoxContainer/NextLevelLabel.rect_size
+	$SuccessScreen/CenterContainer/VBoxContainer/RestartLabel.rect_pivot_offset = 0.5*$SuccessScreen/CenterContainer/VBoxContainer/RestartLabel.rect_size
+	$SuccessScreen/CenterContainer/VBoxContainer/ReturnMainLabel.rect_pivot_offset = 0.5*$SuccessScreen/CenterContainer/VBoxContainer/ReturnMainLabel.rect_size
+	
+	$SuccessScreen/CenterContainer/VBoxContainer/SuccessLabel.set_text(success_text)
+	$SuccessScreen.show()
+
+
+func _on_NextLevelLabel_gui_input(event):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://Scenes/Levels/" + next_level + ".tscn")
 		get_tree().paused = false
